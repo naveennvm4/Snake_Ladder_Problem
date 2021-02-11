669a7fe12b4d59b2c876cbb695e3081a7d8ed91a
naveennvm4 <naveennvm4@gmail.com>
2021-02-11 17:14:48 +0530

    Play until Player reaches winning position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,35 +1,52 @@
-public class SnakeLadder {
-	final static int FINAL_POSITION=100;
-	final static int INITIAL_POSITION=0;
-	final static int LADDER=1;
-	final static int SNAKE=2;
+import java.util.Scanner;
+public class SnakeLadder
+{
+	final static int FINAL_POSITION = 100;
+	final static int INITIAL_POSITION = 0;
+	final static int LADDER = 1;
+	final static int SNAKE = 2;
 	public static void main(String[] args)
 	{
-	int noPlay=0;
-	int position=0;
-	int die=0;
-	int play=(int)(Math.random()*10)%3;
-	System.out.println("chance "+Play);
-	if(play==noPlay)
+	int noPlay = 0;
+	int position = 0;
+	int die = 0;
+	Scanner sc=new Scanner(System.in);
+	String s="START";
+	System.out.println("Start the Game");
+	String st=sc.next();
+	if(s.toLowerCase().toUpperCase().contains(st.toLowerCase().toUpperCase()))
+	while(position<=100)
 	{
-		System.out.println("pass the chance");
-	}
-	else
-	{
-	die = (int) (Math.random()*(6-1+1)+1);
-	System.out.println("Die value ="+die);
-		if(play==LADDER)
+		int play=(int)(Math.random()*10)%3;
+		System.out.println("chance = "+play);
+		if(play==noPlay)
 		{
-		System.out.println("Ladder");
-		position=die+die;
-		System.out.println("Player Position =" +position);
+			System.out.println("No play");
 		}
 		else
 		{
-		System.out.println("Snake");
-		position=die-die;
-		System.out.println("Player Position =" +position);
+			die = (int) (Math.random()*(6-1+1)+1);
+			System.out.println("Die value = "+die);
+			if(play==LADDER)
+			{
+				System.out.println("Ladder");
+				position=position+die;
+				System.out.println("Ladder Position = " +position);
+			}
+			else
+			{
+				System.out.println("Snake");
+				position=position-die;
+				if(position<0)
+				{
+					System.out.println("Snake Position = " +position);
+				}
+				else
+				{
+					System.out.println("Position = " +position);
+				}
+			}
+			}
 		}
-	}
 	}
 }
