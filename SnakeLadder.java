89e5089503181807151fbea91aa3c90b7d1b677f
naveennvm4 <naveennvm4@gmail.com>
2021-02-11 16:26:33 +0530

    Start the game Player position is 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,8 @@
-public class SnakeLadder{
+public class SnakeLadder {
+	final static int PLAYER_1=0;
 	public static void main(String args[])
 	{
-	System.out.println("Welcome to Snake and Ladder Problem");
+	System.out.println("Start the game");
+	System.out.println("Player1 Position is "+PLAYER_1);
 	}
 }
