381809e5aa54885e81c76eef0bee24a963aba818
naveennvm4 <naveennvm4@gmail.com>
2021-02-11 16:33:10 +0530

    Roll the die to get a value between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,10 @@ public class SnakeLadder {
 	final static int PLAYER_1=0;
 	public static void main(String args[])
 	{
+	int value;
 	System.out.println("Start the game");
-	System.out.println("Player1 Position is "+PLAYER_1);
+	System.out.println("Player1 Position is"+PLAYER_1);
+	Value = (int)(Math.random() *(6-1+1) +1);
+	System.out.println("Roll the die ="+ value);
 	}
 }
